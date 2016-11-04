f262d5c4e1143598b71ed249b3433d31c8972813
matt <matt@GQF650>
2016-11-04 11:44:29 +0800

    test

--- index.php
+++ index.php
@@ -5,16 +5,18 @@
  * Some rights reserved：www.simplewind.net
  */
 if (ini_get('magic_quotes_gpc')) {
-	function stripslashesRecursive(array $array){
+
+	function stripslashesRecursive(array $array) {
 		foreach ($array as $k => $v) {
-			if (is_string($v)){
+			if (is_string($v)) {
 				$array[$k] = stripslashes($v);
-			} else if (is_array($v)){
+			} else if (is_array($v)) {
 				$array[$k] = stripslashesRecursive($v);
 			}
 		}
 		return $array;
 	}
+
 	$_GET = stripslashesRecursive($_GET);
 	$_POST = stripslashesRecursive($_POST);
 }
@@ -25,11 +27,11 @@ define('SITE_PATH', "./");
 //项目路径，不可更改
 define('APP_PATH', SITE_PATH . 'application/');
 //项目相对路径，不可更改
-define('SPAPP_PATH',   SITE_PATH.'simplewind/');
+define('SPAPP_PATH', SITE_PATH . 'simplewind/');
 //
-define('SPAPP',   './application/');
+define('SPAPP', './application/');
 //项目资源目录，不可更改
-define('SPSTATIC',   SITE_PATH.'statics/');
+define('SPSTATIC', SITE_PATH . 'statics/');
 //定义缓存存放路径
 define("RUNTIME_PATH", SITE_PATH . "/data/runtime/");
 //版本号
@@ -37,10 +39,10 @@ define("SIMPLEWIND_CMF_VERSION", 'X1.5.0');
 
 define("THINKCMF_CORE_TAGLIBS", 'cx,Common\Lib\Taglib\TagLibSpadmin,Common\Lib\Taglib\TagLibHome');
 
-if(function_exists('saeAutoLoader') || isset($_SERVER['HTTP_BAE_ENV_APPID'])){
+if (function_exists('saeAutoLoader') || isset($_SERVER['HTTP_BAE_ENV_APPID'])) {
 	
-}else{
-	if(file_exists("install") && !file_exists("install/install.lock")){
+} else {
+	if (file_exists("install") && !file_exists("install/install.lock")) {
 		header("Location:./install");
 		exit();
 	}
@@ -48,10 +50,10 @@ if(function_exists('saeAutoLoader') || isset($_SERVER['HTTP_BAE_ENV_APPID'])){
 //uc client root
 define("UC_CLIENT_ROOT", './api/uc_client/');
 
-if(file_exists(UC_CLIENT_ROOT."config.inc.php")){
-	include UC_CLIENT_ROOT."config.inc.php";
+if (file_exists(UC_CLIENT_ROOT . "config.inc.php")) {
+	include UC_CLIENT_ROOT . "config.inc.php";
 }
 
 //载入框架核心文件
-require SPAPP_PATH.'Core/ThinkPHP.php';
+require SPAPP_PATH . 'Core/ThinkPHP.php';
 
